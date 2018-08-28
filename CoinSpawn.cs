be6e71689fbf719bb4f1355793db2e77915e47ee
YSH87 <yshtoo87@gmail.com>
2018-08-28 16:28:16 +0900

    CoinSpawn
    Random CoinSpawn

---
+++ CoinSpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+    public class CoinSpawn : MonoBehaviour
+    {
+		public GameObject[] spawnPos;
+        
+        // random CoinSpawn
+        public void Coin(bool a)
+        {
+			if(a == true)
+            {
+                for (int i = 0; i < 25; i++)
+                {
+                    int random = Random.Range(0,spawnPos.Length);
+                    spawnPos[random].SetActive(true);		
+                }
+            }
+            if(a != true)
+            {
+                for (int i = 0; i < spawnPos.Length; i++)
+                {
+                    spawnPos[i].SetActive(false);		
+                }
+            }
+        }
+    }
