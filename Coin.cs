9e590e2b5d90a6de69b2b22961376c93566e49d6
YSH87 <yshtoo87@gmail.com>
2018-08-29 00:23:07 +0900

    Add Coin

---
+++ Coin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+	[HideInInspector]
+	public int coin;
+
+	public Text coinText;
+
+	private void Awake()
+	{
+		coin = PlayerPrefs.GetInt("Coin");
+		UIUpdate();
+	}
+
+	public void GoldAdd(int add)
+	{
+		coin += add;
+		PlayerPrefs.SetInt("Coin", coin);
+		UIUpdate();
+	}
+
+	public void GoldMinus(int minusGold)
+	{
+		if(coin >= minusGold)
+		{
+			coin -= minusGold;
+		}
+		PlayerPrefs.SetInt("Coin", coin);
+		UIUpdate();
+	}
+
+	public void UIUpdate()
+	{
+		coinText.text = "Coin : " + coin;
+	}
+
+	public void GoldReset()
+	{
+		PlayerPrefs.DeleteKey("Coin");
+	}
+
+}
