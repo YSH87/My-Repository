ac05b2f3abd9a29e9c78696daafa9432bb54da03
YSH87 <yshtoo87@gmail.com>
2018-08-31 16:07:04 +0900

    Rotate

---
+++ Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	public float speed = 45f;
+
+	void Update ()
+	{
+		transform.Rotate(speed * Time.deltaTime,speed * Time.deltaTime,speed  * Time.deltaTime);
+	}
+}
