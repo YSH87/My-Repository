22742832290d2b8b7c7d5f848f1d9d18167ef24f
YSH87 <yshtoo87@gmail.com>
2018-09-02 21:30:50 +0900

    단순이동
    velocity 값을 적용한 이동

---
+++ C#_SourceCode/Move1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move1 : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody playerRigibody;
+	public GameManager gameManager;
+
+	void Start (){
+		playerRigibody = GetComponent<Rigidbody>();
+	}
+	
+	void Update (){
+		if(gameManager.isGameOver == true)
+		{
+			return;
+		}
+		
+		float inputX = Input.GetAxis("Horizontal");
+		float inputZ = Input.GetAxis("Vertical");
+		float fallSpeed = playerRigibody.velocity.y;
+
+		Vector3 velocity = new Vector3(inputX,0,inputZ);
+		velocity = velocity * speed;
+		velocity.y = fallSpeed;
+
+		playerRigibody.velocity = velocity;
+	}
+}
